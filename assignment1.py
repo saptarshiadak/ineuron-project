d9225415ddcfe17810ba067f65ab7bf5a993cfdd
saptarshiadak <104151965+saptarshiadak@users.noreply.github.com>
2022-05-20 19:51:07 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Quistion 1
+
+# In[5]:
+
+
+b= 'hello'
+print(type(b))
+
+
+# In[9]:
+
+
+c= -87.8
+print (type(c))
+
+
+# In[11]:
+
+
+d= '-'
+print(type(d))
+
+
+# In[13]:
+
+
+e= "/,+"
+print(type(e))
+
+
+# In[14]:
+
+
+f=6
+print(type(f))
+
+
+# # Quistion 2
+
+# In[ ]:
+
+
+string a class -  reference datatype it is used to represent text type data.
+variable is any alphabet or gropu of alphabet which work as temporary storage locations , 
+a variable is used to represent any value
+
+
+# # Quistion 3
+
+# In[1]:
+
+
+#Integer Value
+a=1548
+print(type(a))
+
+
+# In[2]:
+
+
+#floot Value
+b=12.548
+print(type(b))
+
+
+# In[3]:
+
+
+#Boolian Value
+c=True
+print(type(c))
+
+
+# In[4]:
+
+
+#list Value
+d=[1,2,3,4]
+print(type(d))
+
+
+# # Quistion 4
+
+# In[6]:
+
+
+# Constant Expressions
+x = 18 +11.3
+  
+print(x)
+
+
+# In[7]:
+
+
+# Arithmetic Expressions
+x = 10
+y = 5
+  
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+  
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+
+
+# In[9]:
+
+
+# Integral Expressions
+a = 10
+b = 9.0
+  
+c = a + int(b)
+print(c)
+
+
+# In[10]:
+
+
+# Multi-operator expression
+  
+a = 10 + 2 * 2
+print(a)
+  
+b = (10 + 5) * 4
+print(b)
+  
+c = 10 + (5 * 8)
+print(c)
+
+
+# # Quistion 5
+
+# In[ ]:
+
+
+expression is made up of values, containers, and mathematical operators.
+statement is just like a command that a python interpreter executes like print.
+
+
+# # Quistion6
+
+# In[11]:
+
+
+bacon=12
+bacon+1
+
+
+# # Quistion 7
+
+# In[12]:
+
+
+'spam'+'spamspam'
+
+
+# In[13]:
+
+
+'spam'*3
+
+
+# # Quistion 8
+
+# In[1]:
+
+
+#In variable name, no special characters allowed other than underscore (_).
+#example:-
+egg=23
+print(egg)
+
+
+# In[2]:
+
+
+_egg=23
+print(_egg)
+
+
+# 100egg=23
+# print(100egg)
+
+# # Quistion 9
+
+# In[ ]:
+
+
+str(), int(), float()
+
+
+# # Quistion 10
+
+# In[2]:
+
+
+99 is an integer it cannot be concatenated with strings, if we have to concatenate it we need to do typecasting.
+   example-
+   
+
+
+# In[3]:
+
+
+print("I have eaten  99  burritos")
+
